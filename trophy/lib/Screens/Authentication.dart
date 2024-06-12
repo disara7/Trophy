a1d106be8a0262919e9545da6a5380137c824890
DushyanthaAT <dushyantha721@gmail.com>
2024-06-12 17:20:26 +0530

    Correct login screen scrolling

--- trophy/lib/Screens/Authentication.dart
+++ trophy/lib/Screens/Authentication.dart
@@ -90,115 +90,121 @@ class _AuthPageState extends State<AuthPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 660,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFFDFEFF),
-                  image: DecorationImage(
-                    image: AssetImage('assets/Login_img.png'),
-                    fit: BoxFit.cover,
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: size.width,
+                  height: size.height / 1.8,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.bottomCenter,
+                    clipBehavior: Clip.antiAlias,
+                    child: Image.asset('assets/Login_img.png'),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40, bottom: 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        labelStyle: const TextStyle(color: Colors.grey),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Color(0xFFF09C46),
+                const SizedBox(height: 10),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 40, right: 40, bottom: 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
                           ),
-                          borderRadius: BorderRadius.circular(12.0),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFFF09C46),
+                            ),
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          prefixIcon: const Icon(Icons.person),
                         ),
-                        prefixIcon: const Icon(Icons.person),
+                        style: const TextStyle(color: Colors.black),
+                        onChanged: (text) {
+                          setState(() {
+                            // Trigger a rebuild when the text changes
+                          });
+                        },
                       ),
-                      style: const TextStyle(color: Colors.black),
-                      onChanged: (text) {
-                        setState(() {
-                          // Trigger a rebuild when the text changes
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 8),
-                    TextField(
-                      obscureText: _obscurePassword,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: const TextStyle(color: Colors.grey),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Color(0xFFF09C46),
+                      const SizedBox(height: 8),
+                      TextField(
+                        obscureText: _obscurePassword,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
                           ),
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        prefixIcon: const Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscurePassword
-                                ? Icons.visibility_off
-                                : Icons.visibility,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0xFFF09C46),
+                            ),
+                            borderRadius: BorderRadius.circular(12.0),
                           ),
-                          onPressed: _togglePasswordVisibility,
+                          prefixIcon: const Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscurePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                            onPressed: _togglePasswordVisibility,
+                          ),
                         ),
+                        style: const TextStyle(color: Colors.black),
+                        onChanged: (text) {
+                          setState(() {});
+                        },
                       ),
-                      style: const TextStyle(color: Colors.black),
-                      onChanged: (text) {
-                        setState(() {});
-                      },
-                    ),
-                    const SizedBox(height: 0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: _showForgotPasswordBottomSheet,
-                          child: const Text(
-                            'Forgot password?',
-                            style: TextStyle(color: Colors.black),
+                      const SizedBox(height: 0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                            onPressed: _showForgotPasswordBottomSheet,
+                            child: const Text(
+                              'Forgot password?',
+                              style: TextStyle(color: Colors.black),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Login',
-                          style: const TextStyle(color: Colors.black),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFF09C46),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                        ],
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Login',
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFF09C46),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 20,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
