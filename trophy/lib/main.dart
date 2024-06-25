4b35faf6b2848bf279dc703534c5f79f52d74628
disara7 <129250994+disara7@users.noreply.github.com>
2024-06-25 22:15:20 +0530

    updated coins page

--- trophy/lib/main.dart
+++ trophy/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:trophy/coinBank/coinbank.dart';
 // import 'package:trophy/coinBank/coinbank.dart';
 // import 'package:trophy/navBar/navbar.dart';
 import 'package:trophy/splash/intro.dart';
 // import 'package:trophy/splash/splash.dart';
 // import 'package:trophy/Screens/Authentication.dart';
 import 'package:trophy/themes/theme.dart';
-
 
 void main() {
   runApp(const MainApp());
@@ -21,7 +21,7 @@ class MainApp extends StatelessWidget {
       theme: TrophyTheme.lightTheme,
       darkTheme: TrophyTheme.darkTheme,
       themeMode: ThemeMode.light,
-      home: const IntroScreen(),
+      home: const CoinBank(),
     );
   }
 }
