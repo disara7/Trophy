3cd037b7e90477e606e7a6a1f595dacc53716e93
disara7 <129250994+disara7@users.noreply.github.com>
2024-06-26 10:42:07 +0530

    Update redeem.dart

--- trophy/lib/coinBank/redeem.dart
+++ trophy/lib/coinBank/redeem.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 class RedeemPage extends StatelessWidget {
   const RedeemPage({super.key});
@@ -10,109 +9,57 @@ class RedeemPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('REDEEM COINS'),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
+      body: Center(
+        child: Container(
+          width: 350, // Set width as needed
+          padding: EdgeInsets.all(0.0), // Add padding inside the container
+          child: Stack(
             children: [
-              Container(
-                width: 350, // Set width as needed
-                padding:
-                    EdgeInsets.all(0.0), // Add padding inside the container
-                child: Stack(
+              Positioned(
+                left: 0,
+                top: 40,
+                child: Image.asset(
+                  'assets/redeemcat.png',
+                  width: 350,
+                  // height: 250, // Adjust image height
+                ),
+              ),
+              Positioned(
+                left: 120,
+                top: 60,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Positioned(
-                      left: 0,
-                      top: 40,
-                      child: Image.asset(
-                        'assets/redeemcat.png',
-                        width: 350,
-                      ),
-                    ),
-                    Positioned(
-                      left: 120,
-                      top: 60,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'The effort pays off!',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.orange,
-                            ),
+                    Container(
+                      width: 220, // Set the desired width
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            'It’s about time you make use of the coins you’ve collected. Redeem the coins you have collected at the checkout of the following.',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'The effort pays off!',
+                              style: TextStyle(
+                                color: Colors.orange,
+                                fontWeight: FontWeight.normal,
+                              ),
                             ),
-                          ),
-                        ],
+                            TextSpan(
+                              text:
+                                  ' It’s about time you make use of the coins you’ve collected. Redeem the coins you have collected at the checkout of the following.',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 20), // Add space between content and carousel
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 200,
-                  enableInfiniteScroll: true,
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  scrollDirection: Axis.horizontal,
-                  viewportFraction: 0.8,
-                  // nextPageDuration: Duration(milliseconds: 500),
-                ),
-                items: [1, 2, 3, 4].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/canteen.png'),
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Item $i',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.orange,
-                                ),
-                              ),
-                              SizedBox(height: 8), // Add space between texts
-                              Text(
-                                'Redeem this item with your coins!',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
               ),
             ],
           ),
