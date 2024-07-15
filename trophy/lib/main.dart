7230186b4c361cb9f0acad3f6c4dc7030880f60d
DushyanthaAT <dushyantha721@gmail.com>
2024-07-16 00:13:32 +0530

    errorFix

--- trophy/lib/main.dart
+++ trophy/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trophy/Screens/Therapy/therapy_home.dart';
 import 'package:trophy/coinBank/coinbank.dart';
 
 import 'package:trophy/coinBank/redeem.dart';
+
 // import 'package:trophy/coinBank/coinbank.dart';
 // import 'package:trophy/navBar/navbar.dart';
 // import 'package:trophy/splash/splash.dart';
 // import 'package:trophy/Screens/Authentication.dart';
 import 'package:trophy/themes/theme.dart';
+
+import 'blocs/therapy/category/category_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -22,7 +27,10 @@ class MainApp extends StatelessWidget {
       theme: TrophyTheme.lightTheme,
       darkTheme: TrophyTheme.darkTheme,
       themeMode: ThemeMode.light,
-      home: CoinBank(),
+      home: BlocProvider(
+        create: (context) => CategoryBloc(),
+        child: CategoryPage(),
+      ),
     );
   }
 }
