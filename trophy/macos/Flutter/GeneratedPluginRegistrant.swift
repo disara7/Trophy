71cdb1a1e9caa87b37f30b6ea9a1b53114c071d4
DushyanthaAT <dushyantha721@gmail.com>
2024-08-07 14:02:09 +0530

    Update GeneratedPluginRegistrant.swift

--- trophy/macos/Flutter/GeneratedPluginRegistrant.swift
+++ trophy/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
 import path_provider_foundation
 import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
