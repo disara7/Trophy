ce0ff86ca885e9c58131bbc71479125184d9874b
disara7 <129250994+disara7@users.noreply.github.com>
2024-09-19 14:17:12 +0530

    linked buttons in home

--- trophy/lib/Screens/home.dart
+++ trophy/lib/Screens/home.dart
@@ -3,11 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:trophy/Components/activity_button.dart';
 import 'package:trophy/Components/button.dart';
+import 'package:trophy/Screens/Therapy/therapy_home.dart';
+import 'package:trophy/Screens/activities.dart';
+import 'package:trophy/Screens/blog.dart';
+import 'package:trophy/Screens/hackathon.dart';
+import 'package:trophy/Screens/sports.dart';
 import 'package:trophy/blocs/home/bloc.dart';
 import 'package:trophy/blocs/home/event.dart';
 import 'package:trophy/blocs/home/state.dart';
 
 import 'package:trophy/Components/custom_app_bar.dart';
+import 'package:trophy/coinBank/coinbank.dart';
 import 'package:trophy/navBar/navbar.dart';
 
 class Home extends StatefulWidget {
@@ -193,6 +199,12 @@ class _HomeState extends State<Home> {
                                 'Activities',
                                 'assets/activity2.png',
                                 onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Activities(), // Navigate to CoinBankPage
+                                    ),
+                                  );
 
                                 },
                               ),
@@ -201,6 +213,12 @@ class _HomeState extends State<Home> {
                                 'Hackathons',
                                 'assets/activity3.png',
                                 onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Hackathon(), // Navigate to CoinBankPage
+                                    ),
+                                  );
 
                                 },
                               ),
@@ -215,6 +233,12 @@ class _HomeState extends State<Home> {
                                 'assets/activity4.png',
                                 iconSize: 27.0,
                                 onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Blog(), // Navigate to CoinBankPage
+                                    ),
+                                  );
 
                                 },
                               ),
@@ -223,6 +247,12 @@ class _HomeState extends State<Home> {
                                 'Sports',
                                 'assets/activity5.png',
                                 onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Sports(), // Navigate to CoinBankPage
+                                    ),
+                                  );
 
                                 },
                               ),
@@ -231,6 +261,12 @@ class _HomeState extends State<Home> {
                                 'Therapy',
                                 'assets/activity6.png',
                                 onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => CategoryPage(), // Navigate to CoinBankPage
+                                    ),
+                                  );
 
                                 },
                               ),
@@ -253,6 +289,12 @@ class _HomeState extends State<Home> {
                           textColor: const Color(0xff131212),
                           text: 'COIN BANK',
                           onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CoinBank(), // Navigate to CoinBankPage
+                              ),
+                            );
 
                           },
                         ),
